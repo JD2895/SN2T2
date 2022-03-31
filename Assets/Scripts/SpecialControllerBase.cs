ffa5e3686c0f686515779f413e46965970760464
Josh Davis <j.davis.2895@gmail.com>
2022-03-30 21:07:51 -0400

    - importing a whole bunch of assets - starting configuration of characters

---
+++ Assets/Scripts/SpecialControllerBase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialControllerBase : MonoBehaviour
+{
+    public virtual void Q_start() { Debug.Log("q"); }
+    public virtual void W_start() { Debug.Log("w"); }
+    public virtual void E_start() { Debug.Log("e"); }
+    public virtual void R_start() { Debug.Log("r"); }
+    public virtual void T_start() { Debug.Log("t"); }
+    public virtual void Y_start() { Debug.Log("y"); }
+    public virtual void U_start() { Debug.Log("u"); }
+
+    public virtual void A_start() { Debug.Log("a"); }
+    public virtual void S_start() { Debug.Log("s"); }
+    public virtual void D_start() { Debug.Log("d"); }
+    public virtual void F_start() { Debug.Log("f"); }
+    public virtual void G_start() { Debug.Log("g"); }
+    public virtual void H_start() { Debug.Log("h"); }
+    public virtual void J_start() { Debug.Log("j"); }
+
+    public virtual void Z_start() { Debug.Log("z"); }
+    public virtual void X_start() { Debug.Log("x"); }
+    public virtual void C_start() { Debug.Log("c"); }
+    public virtual void V_start() { Debug.Log("v"); }
+    public virtual void B_start() { Debug.Log("b"); }
+    public virtual void N_start() { Debug.Log("n"); }
+    public virtual void M_start() { Debug.Log("m"); }
+}
